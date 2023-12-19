26a222a8901e155f4d2fa06d132b4a754cd58485
IsabelleVolaco <isabelletvolaco@gmail.com>
2023-12-19 13:33:06 -0300

    update jwt O erro era:  windows suporta até 259 caracteres

--- Mind-Plus-API-Isabelle/Mind-Plus-API-Isabelle/Contracts/ILoginRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mind_Plus_API_Isabelle.Entity;
-
-namespace Mind_Plus_API_Isabelle.Contracts
-{
-    public interface ILoginRepository
-    {
-        Task<EmployeesEntity> VerifyLogin(string email, string password); 
-    }
-}
